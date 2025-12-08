2011ef79d306b214d207da369f10f293315833d7
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-12-08 23:24:52 +0500

    small changes in js

--- 02Project/script.js
+++ 02Project/script.js
@@ -1,10 +1,27 @@
 function colorChange() {
-    let hexNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    let hexCode = '';
-    for (var i = 0; i < 6; i++) {
-        var random_index = Math.floor(Math.random() * hexNumbers.length);
-        hexCode += hexNumbers[random_index];
-    }
-    document.getElementById('hex-code').innerHTML = hexCode;
-    document.body.style.background = '#' + hexCode;
+  let hexNumbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  let hexCode = "";
+  for (var i = 0; i < 6; i++) {
+    var random_index = Math.floor(Math.random() * hexNumbers.length);
+    hexCode += hexNumbers[random_index];
+  }
+  document.getElementById("hex-code").innerHTML = hexCode;
+  document.body.style.background = "#" + hexCode;
 }
