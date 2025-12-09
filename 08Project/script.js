2b231416db71fb1fe5d7076925789c6763270ca3
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-12-09 19:49:40 +0500

    small changes in js

--- 08Project/script.js
+++ 08Project/script.js
@@ -11,37 +11,37 @@ const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+[]{}|;:',.<>?/";
 
 function getRandomChar(chars) {
-    return chars[Math.floor(Math.random() * chars.length)];
+  return chars[Math.floor(Math.random() * chars.length)];
 }
 
 function generatePassword() {
-    const length = passwordLength.value;
-    let availableChars = "";
-    let password = "";
+  const length = passwordLength.value;
+  let availableChars = "";
+  let password = "";
 
-    if (upper.checked) availableChars += upperChars;
-    if (lower.checked) availableChars += lowerChars;
-    if (num.checked) availableChars += numbers;
-    if (symbol.checked) availableChars += symbols;
+  if (upper.checked) availableChars += upperChars;
+  if (lower.checked) availableChars += lowerChars;
+  if (num.checked) availableChars += numbers;
+  if (symbol.checked) availableChars += symbols;
 
-    if (!availableChars) {
-        alert("Please select at least one option!");
-        return;
-    }
+  if (!availableChars) {
+    alert("Please select at least one option!");
+    return;
+  }
 
-    for (let i = 0; i < length; i++) {
-        password += getRandomChar(availableChars);
-    }
+  for (let i = 0; i < length; i++) {
+    password += getRandomChar(availableChars);
+  }
 
-    passwordInput.value = password;
+  passwordInput.value = password;
 }
 
 function copyPassword() {
-    const password = passwordInput.value;
-    if (password) {
-        navigator.clipboard.writeText(password);
-        alert("Password copied to clipboard!");
-    } else {
-        alert("No password to copy!");
-    }
+  const password = passwordInput.value;
+  if (password) {
+    navigator.clipboard.writeText(password);
+    alert("Password copied to clipboard!");
+  } else {
+    alert("No password to copy!");
+  }
 }
